e7ceb261c6e683aebf80f7b9b7efbbf34e41d54b
Marcel Macedo <marcel.handock@gmail.com>
2019-11-05 20:18:12 -0400

    Aula 133 Corrigindo sistema alterando de Profiles para Users

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
+  
+  devise_for :users
   devise_for :admins
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # se quiser que abra pela palavra inicio
   get 'inicio', to: 'site/welcome#index'
